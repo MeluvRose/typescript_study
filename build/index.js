b08b6a231fa01c50f3d0602c510b8958b3ba2969
meluvrose <tanggu3910@gmail.com>
2023-02-17 14:57:24 +0900

    #5.1 Targets

---
+++ build/index.js
@@ -0,0 +1,9 @@
+var Block = /** @class */ (function () {
+    function Block(data) {
+        this.data = data;
+    }
+    Block.hello = function () {
+        console.log("hi");
+    };
+    return Block;
+}());
