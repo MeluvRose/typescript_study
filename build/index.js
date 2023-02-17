b232c492a6f7ae17e0515aa97c14da53954f31a9
Jinskim <50815599+MeluvRose@users.noreply.github.com>
2023-02-17 15:00:10 +0900

    Delete build directory

--- build/index.js
+++
@@ -1,9 +0,0 @@
-var Block = /** @class */ (function () {
-    function Block(data) {
-        this.data = data;
-    }
-    Block.hello = function () {
-        console.log("hi");
-    };
-    return Block;
-}());
